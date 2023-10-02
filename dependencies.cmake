b1b29d3f59849f8e2febf0c0c9c3e9f7fd6e33f1
Joseph Larralde <joseph.larralde@gmail.com>
2023-10-02 12:01:58 +0200

    nothing

--- dependencies.cmake
+++ dependencies.cmake
@@ -4,6 +4,8 @@ FetchContent_Declare(
   avendish
   GIT_REPOSITORY  "https://github.com/josephlarralde/avendish"
   GIT_TAG         79e5ad260d45357bc24f3c074bb4dfc1db0c44ff
+  #GIT_REPOSITORY  "https://github.com/celtera/avendish"
+  #GIT_TAG         beecd9d442c2ac0e7b181385f076423fd999be83
   GIT_PROGRESS    true
 )
 FetchContent_Populate(avendish)
